407dadd8c71ab0dfb432566533878bc860b2021e
Cheng Zhang <i@zhangcheng.org>
2020-08-18 09:03:16 +0800

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'DOORPOST',
   shortName: 'LONDON',
   description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://flotiq-starter-for-projects.herokuapp.com/', // Domain of your site without prefix!
+  url: 'https://blog.zhangcheng.org/', // Domain of your site without prefix!
   prefix: '/',
   author: 'Flotiq Developers',
   twitter: 'flotiq'
