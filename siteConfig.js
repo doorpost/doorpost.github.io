39634f7e9d4dd4e940761f5845579d721fe62e98
Cheng Zhang <i@zhangcheng.org>
2020-08-19 10:41:32 +0800

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'DOORPOST',
   shortName: 'DOORPOST',
-  description: '这是一个尚在调试中的页面，基于 GitHub & Netlify 构建',
+  description: '这是一个正在进行调试的页面，基于 GitHub & Netlify 构建',
   url: 'https://blog.zhangcheng.org/', // Domain of your site without prefix!
   prefix: '/',
   author: 'Flotiq Developers',
