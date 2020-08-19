d649d47c9e2b99e6397746ed9229bbe6ef5cc51c
Cheng Zhang <i@zhangcheng.org>
2020-08-19 13:56:00 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -88,6 +88,10 @@ const Layout = props => {
           Flotiq
         </a>
       </footer>
+      <footer className="site-foot-right">
+       <img src="https://api.netlify.com/api/v1/badges/8104be56-a5dc-4714-a960-7444ebec8717/deploy-status"  alt="status" />
+        </a>
+      </footer>
     </div>
   )
 };
