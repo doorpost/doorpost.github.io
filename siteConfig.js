8afe7aeffd04c70f2da3ea7f328ef792bb761803
Chace Zhang <i@zhangcheng.org>
2022-01-31 23:21:33 +0800

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: 'DOORPOST',
   shortName: 'DOORPOST',
-  description: '这是一个正在进行调试的页面，基于 GitHub & Netlify 构建',
+  description: 'Have a nice day and productive day.',
   url: 'https://blog.zhangcheng.org/', // Domain of your site without prefix!
   prefix: '/',
-  author: 'Flotiq Developers',
-  twitter: 'flotiq'
+  author: 'Developers',
+  twitter: 'realzhangcheng'
 }
